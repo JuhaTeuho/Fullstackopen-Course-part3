fefddd49adb3c22f05fd56ff740e8c8ff0f95989
Juha <juha.teuho@gmail.com>
2021-11-11 18:04:31 +0200

    doine 3.5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   { "id": 1, "name": "Arto Hellas", "number": "040-123456" },
   { "id": 2, "name": "Ada Lovelace", "number": "39-44-5323523" },
@@ -30,9 +32,26 @@ app.get('/api/persons/:id', (req, res) => {
 app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   persons = persons.filter(person => person.id !== id)
-  
+
   res.status(204).end()
 })
+
+const generateId = () => {
+  return Math.round(Math.random() * (999999999 - 111111111) + 111111111)
+}
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  console.log(body)
+
+  const name = body.name
+  const number = body.number
+  const id = generateId()
+  const newPerson = {"id": id, "name": name, "number": number}
+  persons = persons.concat(newPerson)
+  res.json(newPerson)
+})
+
 
 const PORT = 3001
 app.listen(PORT, () => {
